ec3a6bb887f5883b70b3dfd070f5a7ee63d25972
Danilo D'Alessio <danilo.dalessio@gmail.com>
2022-09-02 19:25:43 +0200

    Primopiatto existence

--- src/components/FoodView.jsx
+++ src/components/FoodView.jsx
@@ -279,66 +279,73 @@ const ItemView = (props) => {
                 <h2 className='pl-4 font-bold text-2xl self-center block w-full mb-4'>
                     Secondi
                 </h2>
-                {menu.Primopiatto.map((i) => {
-                    const withVariations = i.options[0].values.length > 1;
+                {menu.Primopiatto &&
+                    menu.Primopiatto.map((i) => {
+                        const withVariations = i.options[0].values.length > 1;
 
-                    return (
-                        <Link to={`/products/${i.handle}`} key={i.shopifyId}>
-                            <div className='min-h-16 w-full p-4 grid grid-cols-4 hover:bg-gray-200 cursor-pointer'>
-                                <div
-                                    className={
-                                        Boolean(i.featuredImage.gatsbyImageData)
-                                            ? "col-start-1 col-end-4"
-                                            : "col-start-1 col-end-5"
-                                    }
-                                >
-                                    <h2 className='text-xl self-center block w-full mb-1'>
-                                        {i.title}
-                                        {Boolean(i.tags.length) &&
-                                            i.tags.map((tag) => (
-                                                <span
-                                                    key={tag}
-                                                    className='bg-gray-200 text-base px-2 py-1 mx-2 rounded-lg break-words'
-                                                >
-                                                    {tag}
-                                                </span>
-                                            ))}
-                                    </h2>
+                        return (
+                            <Link
+                                to={`/products/${i.handle}`}
+                                key={i.shopifyId}
+                            >
+                                <div className='min-h-16 w-full p-4 grid grid-cols-4 hover:bg-gray-200 cursor-pointer'>
+                                    <div
+                                        className={
+                                            Boolean(
+                                                i.featuredImage.gatsbyImageData
+                                            )
+                                                ? "col-start-1 col-end-4"
+                                                : "col-start-1 col-end-5"
+                                        }
+                                    >
+                                        <h2 className='text-xl self-center block w-full mb-1'>
+                                            {i.title}
+                                            {Boolean(i.tags.length) &&
+                                                i.tags.map((tag) => (
+                                                    <span
+                                                        key={tag}
+                                                        className='bg-gray-200 text-base px-2 py-1 mx-2 rounded-lg break-words'
+                                                    >
+                                                        {tag}
+                                                    </span>
+                                                ))}
+                                        </h2>
 
-                                    <p className='text-sm text-gray-800'>
-                                        {i.description.length < 300
-                                            ? i.description
-                                            : i.description.slice(0, 300) +
-                                              "..."}
-                                    </p>
-                                    {withVariations ? (
-                                        <span className='bg-black text-white px-2 py-1 rounded-md'>
-                                            {i.options[0].values.length} options
-                                        </span>
-                                    ) : (
-                                        <p className='text-lg font-bold'>
-                                            {
-                                                i.priceRangeV2.maxVariantPrice
-                                                    .amount
-                                            }{" "}
-                                            €
+                                        <p className='text-sm text-gray-800'>
+                                            {i.description.length < 300
+                                                ? i.description
+                                                : i.description.slice(0, 300) +
+                                                  "..."}
                                         </p>
+                                        {withVariations ? (
+                                            <span className='bg-black text-white px-2 py-1 rounded-md'>
+                                                {i.options[0].values.length}{" "}
+                                                options
+                                            </span>
+                                        ) : (
+                                            <p className='text-lg font-bold'>
+                                                {
+                                                    i.priceRangeV2
+                                                        .maxVariantPrice.amount
+                                                }{" "}
+                                                €
+                                            </p>
+                                        )}
+                                    </div>
+                                    {Boolean(i.featuredImage) && (
+                                        <div className='col-start-4 col-end-5 text-right'>
+                                            <div
+                                                className='h-24 w-24 bg-cover rounded-lg inline-block'
+                                                style={{
+                                                    backgroundImage: `url(${i.featuredImage.src})`,
+                                                }}
+                                            />
+                                        </div>
                                     )}
                                 </div>
-                                {Boolean(i.featuredImage) && (
-                                    <div className='col-start-4 col-end-5 text-right'>
-                                        <div
-                                            className='h-24 w-24 bg-cover rounded-lg inline-block'
-                                            style={{
-                                                backgroundImage: `url(${i.featuredImage.src})`,
-                                            }}
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                        </Link>
-                    );
-                })}
+                            </Link>
+                        );
+                    })}
             </div>
             <div className='flex flex-col divide-y-2 divide-gray-300 divide-double'>
                 <h2 className='pl-4 font-bold text-2xl self-center block w-full mb-4'>
