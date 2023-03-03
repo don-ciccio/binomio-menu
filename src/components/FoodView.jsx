a2e1f6f2405da80fc46c7b08a72175e2b6e2dfa0
Danilo D'Alessio <danilo.dalessio@gmail.com>
2023-03-03 17:14:32 +0100

    Tacos

--- src/components/FoodView.jsx
+++ src/components/FoodView.jsx
@@ -147,6 +147,71 @@ const FoodView = (props) => {
             </div>
             <div className='flex flex-col divide-y-2 divide-gray-300 divide-double'>
                 <h2 className='pl-4 font-bold text-3xl self-center block w-full mb-5'>
+                    Tacos
+                </h2>
+                {menu.Tacos.map((i) => {
+                    const withVariations = i.options[0].values.length > 1;
+
+                    return (
+                        <Link to={`/products/${i.handle}`} key={i.shopifyId}>
+                            <div className='min-h-16 w-full p-4 grid grid-cols-4 hover:bg-gray-200 cursor-pointer'>
+                                <div
+                                    className={
+                                        Boolean(i.featuredImage.gatsbyImageData)
+                                            ? "col-start-1 col-end-4"
+                                            : "col-start-1 col-end-5"
+                                    }
+                                >
+                                    <h2 className='font-semibold text-xl self-center block w-full mb-1'>
+                                        {i.title.toUpperCase()}
+                                        {Boolean(i.tags.length) &&
+                                            i.tags.map((tag) => (
+                                                <span
+                                                    key={tag}
+                                                    className='bg-gray-200 text-base px-2 py-1 mx-2 rounded-lg break-words'
+                                                >
+                                                    {tag}
+                                                </span>
+                                            ))}
+                                    </h2>
+
+                                    <p className='text-sm text-gray-800'>
+                                        {i.description.length < 300
+                                            ? i.description
+                                            : i.description.slice(0, 300) +
+                                              "..."}
+                                    </p>
+                                    {withVariations ? (
+                                        <span className='bg-black text-white px-2 py-1 rounded-md'>
+                                            {i.options[0].values.length} options
+                                        </span>
+                                    ) : (
+                                        <p className='text-lg font-bold'>
+                                            {
+                                                i.priceRangeV2.maxVariantPrice
+                                                    .amount
+                                            }{" "}
+                                            €
+                                        </p>
+                                    )}
+                                </div>
+                                {Boolean(i.featuredImage) && (
+                                    <div className='col-start-4 col-end-5 text-right'>
+                                        <div
+                                            className='h-24 w-24 bg-cover rounded-lg inline-block'
+                                            style={{
+                                                backgroundImage: `url(${i.featuredImage.src})`,
+                                            }}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+            <div className='flex flex-col divide-y-2 divide-gray-300 divide-double'>
+                <h2 className='pl-4 font-bold text-3xl self-center block w-full mb-5'>
                     Hamburger
                 </h2>
                 {menu.Hamburger.map((i) => {
